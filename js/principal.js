21600cc98c209a6284d021b46fb1aeecf85f4ec5
Edivan <edivanlima23@gmail.com>
2020-11-15 19:51:59 -0300

    adicionando pacientes na tabela

--- js/principal.js
+++ js/principal.js
@@ -36,10 +36,35 @@ for (var i = 0; i < pacientes.length; i++) {
   }
 }
 
-var pacientes = document.querySelector(".paciente");
-console.log(pacientes);
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event){
+  event.preventDefault();
 
-var pacientes = document.querySelectorAll(".paciente");
-console.log(pacientes);
+  var form = document.querySelector("#form-adicionar");
 
-console.log("Hola , mundo!");
+  var nome = form.nome.value;
+  var peso= form.peso.value;
+  var altura = form.altura.value;
+  var gordura =form.gordura.value;
+  pacienteTr = document.createElement("tr");
+
+var nomeTd = document.createElement("td")
+var pesoTd = document.createElement("td")
+var alturaTd = document.createElement("td")
+var gorduraTd = document.createElement("td")
+var imcTd = document.createElement("td")
+nomeTd.textContent= nome;
+pesoTd.textContent= peso;
+alturaTd.textContent= altura;
+gorduraTd.textContent= gordura;
+
+pacienteTr.appendChild(nomeTd)
+pacienteTr.appendChild(pesoTd)
+pacienteTr.appendChild(alturaTd)
+pacienteTr.appendChild(gorduraTd)
+console.log(pacienteTr);
+
+var tabela = document.querySelector("#tabela-pacientes")
+tabela.appendChild(pacienteTr);
+
+});
