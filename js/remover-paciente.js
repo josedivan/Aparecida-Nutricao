865370a356939564a01b6110ef4284349b6e432c
Edivan <edivanlima23@gmail.com>
2020-11-20 18:31:26 -0300

    delegação e animaçao

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,10 +1,14 @@
-var pacientes = document.querySelectorAll(".paciente");
+
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event){
-  var alvoEvento = event.target;
-  var paiDoAlvo = alvoEvento.parentNode;
-paiDoAlvo.remove();
+  event.target.parentNode.classList.add("fadeOut");
+
+
+setTimeout(function(){
+  event.target.parentNode.remove();
+},500);
+
 });
 
 
