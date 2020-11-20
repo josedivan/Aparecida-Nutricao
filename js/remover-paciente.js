f47c5bc857a495d1e6760e3e476471ceb9029be4
Edivan <edivanlima23@gmail.com>
2020-11-19 22:17:56 -0300

    removendo pacientes da tabela

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,9 +1,22 @@
 var pacientes = document.querySelectorAll(".paciente");
-pacientes.forEach(function(paciente){
-  paciente.addEventListener("dblclick", function(){
-    console.log(" fui clicado");
-    this.remove();
+var tabela = document.querySelector("table");
 
-  });
-
+tabela.addEventListener("dblclick", function(event){
+  var alvoEvento = event.target;
+  var paiDoAlvo = alvoEvento.parentNode;
+paiDoAlvo.remove();
 });
+
+
+
+
+
+//pacientes.forEach(function(paciente){
+ // paciente.addEventListener("dblclick", function(){
+    //console.log(" fui clicado");
+    //this.remove();
+    
+
+  //})
+
+//});
